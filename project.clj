082dcd79f8da3cad1fd35e9c21590bba9a890aa6
Ryan McGowan <ryan@ryanmcg.com>
2012-09-02 23:39:23 -0400

    Adds clj-ml dependency.

--- project.clj
+++ project.clj
@@ -3,4 +3,8 @@
   :url "https://github.com/DRSNJM/board-ultimatum-engine"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :scm {:name "git"
+        :tag "163f4843fb344971d9828400ea62ae5e317cd168"
+        :url "git://github.com/DRSNJM/board-ultimatum-engine.git"}
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [com.leadtune/clj-ml "0.2.1"]])
