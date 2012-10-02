fd7da7aef649087a186a35a4014f6ce7bd636952
dalbz <david.albert09@gmail.com>
2012-10-02 14:25:41 -0400

    Add rudimentary rules and query example to the logic engine

--- src/board_ultimatum/engine.clj
+++ src/board_ultimatum/engine.clj
@@ -1,6 +1,22 @@
-(ns board-ultimatum.engine)
+(ns board-ultimatum.engine
+    (:refer-clojure :exclude [==])
+    (:use [clojure.core.logic]))
 
 (defn foo
   "I don't do a whole lot."
   [x]
   (println x "Hello, World!"))
+
+(defrel num-players game n)
+
+(facts num-players [['Checkers 2]
+                    ['Chess 2]
+                    ['Solitaire 1]])
+
+(defn query []
+    (run* [q]
+        (num-players 'Checkers q)))
+
+(defn query2 []
+    (run* [q]
+        (num-players q 2)))
