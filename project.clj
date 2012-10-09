0ad92512adbdaea959caed7b9637fd26000806f1
Ryan McGowan <ryan@ryanmcg.com>
2012-10-09 01:41:12 -0400

    Add data.zip dependency.

--- project.clj
+++ project.clj
@@ -8,5 +8,6 @@
         :url "git://github.com/DRSNJM/board-ultimatum-engine.git"}
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/core.logic "0.7.5"]
+                 [org.clojure/data.zip "0.1.1"]
                  [com.novemberain/monger "1.1.2"]
                  [com.leadtune/clj-ml "0.2.4"]])
