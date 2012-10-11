552efc47e67ec05ca758d11bde8364eb1907b9d6
dalbz <david.albert09@gmail.com>
2012-10-11 15:57:16 -0400

    Construct a giant network ala what we are going to need for the board game similarity monstrosity

--- src/board_ultimatum/neural.clj
+++ src/board_ultimatum/neural.clj
@@ -1,2 +1,9 @@
 (ns board-ultimatum.neural
     (use [enclog nnets training]))
+
+(def net  
+  (network  (neural-pattern :feed-forward) 
+    :activation :sigmoid 
+    :input   200
+    :output  1
+    :hidden [200 100])) 
