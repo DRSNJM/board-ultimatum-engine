6036f1c6914d09f25599521a675014281b747c44
Ryan McGowan <ryan@ryanmcg.com>
2012-10-16 19:01:26 -0400

    Do not authenticate on connect without a username.

--- src/board_ultimatum/engine/model.clj
+++ src/board_ultimatum/engine/model.clj
@@ -5,9 +5,7 @@
             [clojure.java.io :as io]
             [clojure.zip :as zip]
             [clojure.xml :as xml])
-  (:use monger.operators
-        clojure.data.zip.xml
-        [board-ultimatum.engine.config :only [storage]]))
+  (:use monger.operators))
 
 ;; This namespace contains all functions related to manipulating the
 ;; applications "model" (which is mostly mongo).
@@ -20,18 +18,18 @@
 
 (defn connect
   "Connect to mongo based on the given connection information."
-  ([connection-info]
-   (if (:uri connection-info)
-     (mg/connect-via-uri! (:uri connection-info))
-     (let [db-name (:db-name connection-info)]
-       (mg/connect!)
-       (mg/authenticate db-name
-                        (:username connection-info)
-                        (into-array Character/TYPE (:password connection-info)))
-       (mg/set-db! (mg/get-db db-name))))
-   ; Set up the indexes necessary for decent performance.
-   (ensure-indexes))
-  ([] (connect storage)))
+  [connection-info]
+  (if (:uri connection-info)
+    (mg/connect-via-uri! (:uri connection-info))
+    (let [db-name (:db-name connection-info)]
+      (mg/connect!)
+      (when (not (nil? (:username connection-info)))
+        (mg/authenticate db-name
+                         (:username connection-info)
+                         (into-array Character/TYPE (:password connection-info))))
+      (mg/set-db! (mg/get-db db-name))))
+  ; Set up the indexes necessary for decent performance.
+  (ensure-indexes))
 
 (defn add-games
   "Batch inserts the given sequence of games into mongo."
